a484bd9af09397b58ff417bc6a12e19a28240c0c
Abhishek Kashyap <abhishek47kashyap@gmail.com>
2026-08-09 17:03:31 +0200

    renamed topic name

--- src/cpp_pkg/src/listener.cpp
+++ src/cpp_pkg/src/listener.cpp
@@ -2,7 +2,7 @@
 
 Listener::Listener() : Node("listener") {
     subscription_ = create_subscription<std_msgs::msg::String>(
-        "chatter", 10, [this](std_msgs::msg::String::SharedPtr message) {
+        "cpp_chatter", 10, [this](std_msgs::msg::String::SharedPtr message) {
             RCLCPP_INFO(get_logger(), "%s", message->data.c_str());
         });
 }
--- src/cpp_pkg/src/talker.cpp
+++ src/cpp_pkg/src/talker.cpp
@@ -3,7 +3,7 @@
 #include "cpp_pkg/nodes.hpp"
 
 Talker::Talker() : Node("talker") {
-    publisher_ = create_publisher<std_msgs::msg::String>("chatter", 10);
+    publisher_ = create_publisher<std_msgs::msg::String>("cpp_chatter", 10);
     timer_ = create_wall_timer(std::chrono::seconds(1), [this] {
         std_msgs::msg::String message;
         message.data = "Help me Obi-Wan Kenobi, you're my only hope!";
